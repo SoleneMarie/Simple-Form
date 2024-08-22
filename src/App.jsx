fc24f00e7727ea6f01e48d2850aba5522b462e80
SoleneMarie <solene.plassart@gmail.com>
2024-08-22 16:36:08 +0200

    Good but register ??

--- src/App.jsx
+++ src/App.jsx
@@ -19,61 +19,65 @@ function App() {
 
   return (
     <>
-   {stepTwo===false?       {/**-----------------------------------Formulaire---------------------------------------/ */}
-      <form>
-        {/**-----------------------------------Nom---------------------------------------/ */}
-        <label htmlFor="name">Name</label>
-        <input
-          type="texte"
-          name="name"
-          value={name}
-          onChange={(event) => changeField(setName, event)}
-        />
-        {/**-----------------------------------Mail---------------------------------------/ */}
-        <label htmlFor="mail">Email</label>
-        <input
-          type="email"
-          name="mail"
-          value={mail}
-          onChange={(event) => changeField(setMail, event)}
-        />
-        {/**-----------------------------------Password---------------------------------------/ */}
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(event) => changeField(setPassword, event)}
-        />
-        {/**-----------------------------------Checkpassword---------------------------------------/ */}
-        <label htmlFor="checkPass">Confirm your password</label>
-        <input
-          type="password"
-          name="checkPass"
-          value={checkPass}
-          onChange={(event) => changeField(setCheckPass, event)}
-        />
-        {/**-----------------------------------Formulaire---------------------------------------/ */}
-      </form>
-      <button
-        onclick={() => {
-          if (checkPass === password) {
-            {
-              /* charger le formulaire Results */
-            }
-          } else {
-            {
-              /* message d'erreur : les mots de passe ne sont pas identiques */
-            }
-          }
-        }}
-      >
-        Register
-      </button>    : "Salut"      }
-    
- 
-
-      {/**-----------------------------------------------------------------------------------/ */}
+      {stepTwo === false ? (
+        <>
+          {" "}
+          {/**-----------------------------------Formulaire---------------------------------------/ */}
+          <form>
+            {/**-----------------------------------Nom---------------------------------------/ */}
+            <label htmlFor="name">Name</label>
+            <input
+              type="texte"
+              name="name"
+              onChange={(event) => changeField(setName, event)}
+              value={name}
+            />
+            {/**-----------------------------------Mail---------------------------------------/ */}
+            <label htmlFor="mail">Email</label>
+            <input
+              type="email"
+              name="mail"
+              onChange={(event) => changeField(setMail, event)}
+              value={mail}
+            />
+            {/**-----------------------------------Password---------------------------------------/ */}
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              onChange={(event) => changeField(setPassword, event)}
+              value={password}
+            />
+            {/**-----------------------------------Checkpassword---------------------------------------/ */}
+            <label htmlFor="checkPass">Confirm your password</label>
+            <input
+              type="password"
+              name="checkPass"
+              onChange={(event) => changeField(setCheckPass, event)}
+              value={checkPass}
+            />
+            {/**-----------------------------------Formulaire---------------------------------------/ */}
+          </form>
+          <button
+            onClick={() => {
+              if (checkPass === password) {
+                {
+                  setStepTwo(true);
+                }
+              } else {
+                {
+                  /* message d'erreur : les mots de passe ne sont pas identiques */
+                }
+              }
+            }}
+          >
+            Register
+          </button>
+          {/**-----------------------------------------------------------------------------------/ */}
+        </>
+      ) : (
+        "salut"
+      )}
     </>
   );
 }
